823139f7239c8772ba4da07f11ae65af094f869d
codynego <emonenaabednego@gmail.com>
2023-05-25 20:13:59 +0100

    password ecryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ 
+    Returns a salted, hashed password as byte string 
+    """
+    encode = password.encode()
+    hashe = bcrypt.hashpw(encode, bcrypt.gensalt())
+
+    return hashe
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ 
+    Validate the password matches the hashed password 
+    """
+    isvalid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        isvalid = True
+    return isvalid
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ Regex-ing
 import re
 from typing import List
 import logging
+from os import environ
+import mysql.connector
+
 
 
 def filter_datum(fields: List[str],
@@ -43,3 +46,55 @@ class RedactingFormatter(logging.Formatter):
         filter = filter_datum(self.fields, self.REDACTION, record,
                               self.SEPARATOR)
         return filter
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+
+def main():
+    """
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
