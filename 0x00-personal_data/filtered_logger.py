488e9c8e170f12e9310cb1cf5da78addd5abdcbe
codynego <emonenaabednego@gmail.com>
2023-05-25 13:21:07 +0100

    remove extra ;

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,4 +10,4 @@ def filter_datum(fields, redaction, message, separator):
      a function called filter_datum that returns the log message obfuscated
      """
     regex_pattern = r'({})=([^{}]+)'.format('|'.join(map(re.escape, fields)), separator)
-    return re.sub(regex_pattern, r'\1={};'.format(redaction), message)
+    return re.sub(regex_pattern, r'\1={}'.format(redaction), message)
