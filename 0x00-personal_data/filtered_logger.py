bd82b1593121351fa6d8f8bc5616f53b712191be
codynego <emonenaabednego@gmail.com>
2023-05-25 13:24:59 +0100

    fix pep8 error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,9 +5,11 @@ Regex-ing
 """
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
     """
      a function called filter_datum that returns the log message obfuscated
      """
-    regex_pattern = r'({})=([^{}]+)'.format('|'.join(map(re.escape, fields)), separator)
+    regex_pattern = r'({})=([^{}]+)'.format(
+            '|'.join(map(re.escape, fields)), separator)
     return re.sub(regex_pattern, r'\1={}'.format(redaction), message)
