13b1decc1c05c2a118db1a0565ed2937e7d5fce6
codynego <emonenaabednego@gmail.com>
2023-05-25 13:16:30 +0100

    filter logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Regex-ing
+"""
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    """
+     a function called filter_datum that returns the log message obfuscated
+     """
+    regex_pattern = r'({})=([^{}]+)'.format('|'.join(map(re.escape, fields)), separator)
+    return re.sub(regex_pattern, r'\1={};'.format(redaction), message)
